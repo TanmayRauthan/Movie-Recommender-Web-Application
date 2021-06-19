ffc5046e0e45c28cc69f76c01b71c69fdf0526f9
TanmayRauthan <65180191+TanmayRauthan@users.noreply.github.com>
2021-06-19 09:52:59 +0530

    Update settings.py

--- mainmodel/settings.py
+++ mainmodel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-36&r-mh6krbg8y^k4^c20#8b2cv6!3@6b&ho0&sz0cu%1o42t8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','tr-movie-recommender.herokuapp.com']
 
 
 # Application definition
